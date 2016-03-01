8d5bb93c62d8c6357a6199c622789ecdb15d0b0c
yangweixian <yangweixian@xiaomi.com>
2016-03-01 14:52:23 +0800

    up md5.js

--- md5.js
+++ md5.js
@@ -17,51 +17,31 @@ files.forEach(function(elem, i) {
   var data = fs.readFileSync(filePath, 'utf8');
   if (/^.*\.(css|js|html)/.test(elem)) {
     console.log('replace', elem);
-    var newData = data.replace(removeReg, '');
+    data = data.replace(removeReg, '');
 
-    var a = data.replace(new RegExp('sounder.js', 'g'), 'a.js');
-    var b = data.replace(new RegExp('modal.js', 'g'), 'b.js');
-    var c = data.replace(new RegExp('request.js', 'g'), 'c.js');
-    var d = data.replace(new RegExp('brand.js', 'g'), 'd.js');
-    var e = data.replace(new RegExp('gameEngine.js', 'g'), 'e.js');
-    var f = data.replace(new RegExp('bullets.js', 'g'), 'f.js');
-    var g = data.replace(new RegExp('score.js', 'g'), 'g.js');
-    var h = data.replace(new RegExp('stopwatch.js', 'g'), 'h.js');
-    var i = data.replace(new RegExp('animationTimer.js', 'g'), 'i.js');
-    var j = data.replace(new RegExp('caculatePoint.js', 'g'), 'j.js');
-    var k = data.replace(new RegExp('shapes.js', 'g'), 'k.js');
-    var l = data.replace(new RegExp('player.js', 'g'), 'l.js');
-    var m = data.replace(new RegExp('bullet-pea.js', 'g'), 'm.js');
-    var n = data.replace(new RegExp('devil.js', 'g'), 'n.js');
-    var o = data.replace(new RegExp('barrier.js', 'g'), 'o.js');
-    var p = data.replace(new RegExp('rocket.js', 'g'), 'p.js');
-    var q = data.replace(new RegExp('cannon.js', 'g'), 'q.js');
-    var r = data.replace(new RegExp('fps.js', 'g'), 'r.js');
-    var s = data.replace(new RegExp('result.js', 'g'), 's.js');
-    var t = data.replace(new RegExp('index.js', 'g'), 't.js');
+    data = data.replace(new RegExp('sounder.js', 'g'), 'a.js');
+    data = data.replace(new RegExp('modal.js', 'g'), 'b.js');
+    data = data.replace(new RegExp('request.js', 'g'), 'c.js');
+    data = data.replace(new RegExp('brand.js', 'g'), 'd.js');
+    data = data.replace(new RegExp('gameEngine.js', 'g'), 'e.js');
+    data = data.replace(new RegExp('bullets.js', 'g'), 'f.js');
+    data = data.replace(new RegExp('score.js', 'g'), 'g.js');
+    data = data.replace(new RegExp('stopwatch.js', 'g'), 'h.js');
+    data = data.replace(new RegExp('animationTimer.js', 'g'), 'i.js');
+    data = data.replace(new RegExp('caculatePoint.js', 'g'), 'j.js');
+    data = data.replace(new RegExp('shapes.js', 'g'), 'k.js');
+    data = data.replace(new RegExp('player.js', 'g'), 'l.js');
+    data = data.replace(new RegExp('bullet-pea.js', 'g'), 'm.js');
+    data = data.replace(new RegExp('devil.js', 'g'), 'n.js');
+    data = data.replace(new RegExp('barrier.js', 'g'), 'o.js');
+    data = data.replace(new RegExp('rocket.js', 'g'), 'p.js');
+    data = data.replace(new RegExp('cannon.js', 'g'), 'q.js');
+    data = data.replace(new RegExp('fps.js', 'g'), 'r.js');
+    data = data.replace(new RegExp('result.js', 'g'), 's.js');
+    data = data.replace(new RegExp('index.js', 'g'), 't.js');
 
 
-    fs.writeFileSync(filePath, newData, 'utf8');
-    fs.writeFileSync(filePath, a, 'utf8');
-    fs.writeFileSync(filePath, b, 'utf8');
-    fs.writeFileSync(filePath, c, 'utf8');
-    fs.writeFileSync(filePath, d, 'utf8');
-    fs.writeFileSync(filePath, e, 'utf8');
-    fs.writeFileSync(filePath, f, 'utf8');
-    fs.writeFileSync(filePath, g, 'utf8');
-    fs.writeFileSync(filePath, h, 'utf8');
-    fs.writeFileSync(filePath, i, 'utf8');
-    fs.writeFileSync(filePath, j, 'utf8');
-    fs.writeFileSync(filePath, k, 'utf8');
-    fs.writeFileSync(filePath, l, 'utf8');
-    fs.writeFileSync(filePath, m, 'utf8');
-    fs.writeFileSync(filePath, n, 'utf8');
-    fs.writeFileSync(filePath, o, 'utf8');
-    fs.writeFileSync(filePath, p, 'utf8');
-    fs.writeFileSync(filePath, q, 'utf8');
-    fs.writeFileSync(filePath, r, 'utf8');
-    fs.writeFileSync(filePath, s, 'utf8');
-    fs.writeFileSync(filePath, t, 'utf8');
+    fs.writeFileSync(filePath, data, 'utf8');
   }
 });
 // 计算md5
